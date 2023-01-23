b42726f6435737d70a36facc56aa5da24d0e1b47
Grigorich <oskinnick@gmail.com>
2023-01-23 19:47:26 +0300

    Manual commit 2

--- src/main/lesson1/Lesson1.java
+++ src/main/lesson1/Lesson1.java
@@ -1,5 +1,15 @@
 package main.lesson1;
 
 public class Lesson1 {
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
 
+    public static void task1(){
+        System.out.println("Homework done");
+    }
+    public static void task2(){
+        System.out.println("This the Task 2");
+    }
 }

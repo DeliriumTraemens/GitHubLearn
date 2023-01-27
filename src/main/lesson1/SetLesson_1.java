2ed8c948ae8cea9f7ce4dc97f6c689197e1ae647
Grigorich <oskinnick@gmail.com>
2023-01-27 11:40:45 +0300

    Intset added

---
+++ src/main/lesson1/SetLesson_1.java
@@ -0,0 +1,16 @@
+package main.lesson1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetLesson_1 {
+
+    public static void main(String[] args) {
+ Set<Integer> intSet=new HashSet<Integer>();
+        intSet.add(2);
+        intSet.add(4);
+        intSet.add(6);
+
+        intSet.forEach(System.out::println);
+    }
+}

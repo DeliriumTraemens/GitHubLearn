3556e8a040b8b5cb43d1bc276090ec311c57eb13
Grigorich <oskinnick@gmail.com>
2023-01-27 11:43:07 +0300

    String LinkedList added

--- src/main/lesson1/SetLesson_1.java
+++ src/main/lesson1/SetLesson_1.java
@@ -1,16 +1,20 @@
 package main.lesson1;
 
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 public class SetLesson_1 {
 
     public static void main(String[] args) {
- Set<Integer> intSet=new HashSet<Integer>();
+        Set<Integer> intSet = new HashSet<Integer>();
         intSet.add(2);
         intSet.add(4);
         intSet.add(6);
         intSet.add(8);
+
+        List<String> strList = new LinkedList<String>();
 
         intSet.forEach(System.out::println);
     }
